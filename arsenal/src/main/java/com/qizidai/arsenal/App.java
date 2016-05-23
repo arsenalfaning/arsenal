6bff68980396cb77e7bbb5f03cd4ff2eb78f31e2
arsenalfaning <765205025@qq.com>
2016-05-23 23:07:29 +0800

    socket通信测试

--- arsenal/src/main/java/com/qizidai/arsenal/App.java
+++ arsenal/src/main/java/com/qizidai/arsenal/App.java
@@ -1,5 +1,9 @@
 package com.qizidai.arsenal;
 
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+			java.net.ServerSocket serverSocket = new ServerSocket(8080);
+			Random random = new Random();
+			while(true) {
+				java.net.Socket socket = serverSocket.accept();
+				PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+				pw.println("<!doctype html>");
+				pw.println("<html><head>                 <meta http-equiv=Content-Type content=\"text/html;charset=utf-8\"><meta http-equiv=X-UA-Compatible content=\"IE=edge,chrome=1\"></head>");
+				pw.println("<body>" + random.nextInt(100) + "</body>");
+				pw.println("</html>");
+				pw.close();
+			}
+        } catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 }
